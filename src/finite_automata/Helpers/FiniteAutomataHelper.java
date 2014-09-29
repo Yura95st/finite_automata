ec777a8c30c3207548ee86812524a77812d1658c
Yura95st <yura95st@gmail.com>
2014-09-30 00:00:29 +0300

    Adds beta implementation of the algorithm for getting all words, accepted by the finite automata.

--- src/finite_automata/Helpers/FiniteAutomataHelper.java
+++ src/finite_automata/Helpers/FiniteAutomataHelper.java
@@ -1,9 +1,11 @@
 package finite_automata.Helpers;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import finite_automata.FiniteAutomata;
 import finite_automata.IFiniteAutomata;
@@ -25,7 +27,7 @@ public class FiniteAutomataHelper
 		{
 			throw new IllegalArgumentException();
 		}
-
+		
 		List<String> list = new ArrayList<String>()
 		{
 			{
@@ -40,42 +42,42 @@ public class FiniteAutomataHelper
 								.getTransitionsMap()));
 			}
 		};
-
-				return list;
+		
+		return list;
 			}
-
+	
 	private static String convertFiniteStatesListToString(
 			List<Integer> finiteStates)
 	{
 		int finiteStatesCount = finiteStates.size();
-
+		
 		StringBuilder stringBuilder = new StringBuilder();
-
+		
 		stringBuilder.append(finiteStatesCount);
-
+		
 		for (int i = 0; i < finiteStatesCount; i++)
 		{
 			stringBuilder.append(" ");
 			stringBuilder.append(finiteStates.get(i));
 		}
-
+		
 		String finiteStatesString = stringBuilder.toString();
-
+		
 		return finiteStatesString;
 	}
-
+	
 	private static String convertTransitionsMapToString(
 			Map<Integer, List<Transition>> transitionsMap)
 	{
 		StringBuilder stringBuilder = new StringBuilder();
-
+		
 		Iterator iterator = transitionsMap.entrySet().iterator();
-
+		
 		while (iterator.hasNext())
 		{
 			Map.Entry<Integer, List<Transition>> entry = (Map.Entry<Integer, List<Transition>>) iterator
 					.next();
-
+			
 			for (Transition transition : entry.getValue())
 			{
 				stringBuilder.append(transition.getFromState());
@@ -86,12 +88,90 @@ public class FiniteAutomataHelper
 				stringBuilder.append(System.getProperty("line.separator"));
 			}
 		}
-
+		
 		String transitionsMapString = stringBuilder.toString().trim();
-
+		
 		return transitionsMapString;
 	}
+	
+	/**
+	 * Gets the list of words, accepted by the automata
+	 *
+	 * @param finiteAutomata
+	 * @return
+	 */
+	public static List<String> getAllAcceptedWords(
+			final IFiniteAutomata finiteAutomata)
+	{
+		System.out.println("======");
 
+		List<String> words = new ArrayList<String>();
+		
+		List<Transition> visited = new ArrayList<Transition>();
+
+		Stack<Map.Entry<String, List<Transition>>> stack = new Stack<Map.Entry<String, List<Transition>>>();
+		
+		// Initial step
+		List<Transition> initialTransitions = finiteAutomata
+				.getTransitionsMap().get(finiteAutomata.getInitialState());
+		
+		stack.push(new AbstractMap.SimpleEntry<>("", initialTransitions));
+		
+		while (!stack.isEmpty())
+		{
+			Map.Entry<String, List<Transition>> entry = stack.peek();
+			
+			boolean popFromStack = true;
+			
+			for (Transition transition : entry.getValue())
+			{
+				visited.add(transition);
+
+				List<Transition> children = new ArrayList<Transition>();
+				
+				List<Transition> stateTransitions = finiteAutomata
+						.getTransitionsMap().get(transition.getToState());
+				
+				for (Transition childTransition : stateTransitions)
+				{
+					if (!visited.contains(childTransition))
+					{
+						children.add(childTransition);
+					}
+				}
+				
+				if (children.size() > 0)
+				{
+					String key = entry.getKey() + transition.getCharacter();
+					
+					stack.push(new AbstractMap.SimpleEntry<>(key, children));
+					
+					popFromStack = false;
+				}
+			}
+			
+			if (popFromStack)
+			{
+				stack.pop();
+
+				for (Transition transition : entry.getValue())
+				{
+					if (finiteAutomata.getFiniteStates().contains(
+							transition.getToState()))
+					{
+						String word = entry.getKey()
+								+ transition.getCharacter();
+						words.add(word);
+
+						System.out.println(word);
+					}
+				}
+			}
+		}
+		
+		return words;
+	}
+	
 	/**
 	 * Gets finite automata from the list of strings
 	 *
@@ -104,48 +184,48 @@ public class FiniteAutomataHelper
 					throws FailedToGetFiniteAutomataFromStringListException
 	{
 		int listSize = list.size();
-
+		
 		if (listSize < 5)
 		{
 			throw new FailedToGetFiniteAutomataFromStringListException(
 					"Invalid list format: list must contain at least 5 items.");
 		}
-
+		
 		IFiniteAutomata finiteAutomata = new FiniteAutomata();
-
+		
 		try
 		{
 			finiteAutomata
 			.setAlphabetCardinality(Integer.parseInt(list.get(0)));
-
+			
 			finiteAutomata.setStatesCardinality(Integer.parseInt(list.get(1)));
-
+			
 			finiteAutomata.setInitialState(Integer.parseInt(list.get(2)));
-
+			
 			String[] finiteStatesLine = list.get(3).split(" ");
-
+			
 			// Ignore the first parameter - cardinality of the finiteStates set.
 			if (finiteStatesLine.length <= 1)
 			{
 				throw new FailedToGetFiniteAutomataFromStringListException(
 						"Finite automata must have at least one finite state.");
 			}
-
+			
 			for (int i = 1, count = finiteStatesLine.length; i < count; i++)
 			{
 				finiteAutomata.addFiniteState(Integer
 						.parseInt(finiteStatesLine[i]));
 			}
-
+			
 			for (int i = 4; i < listSize; i++)
 			{
 				String[] transitionLine = list.get(i).split(" ");
-
+				
 				Transition transition = new Transition(
 						Integer.parseInt(transitionLine[0]),
 						transitionLine[1].charAt(0),
 						Integer.parseInt(transitionLine[2]));
-
+				
 				finiteAutomata.addTransition(transition);
 			}
 		}
@@ -158,7 +238,7 @@ public class FiniteAutomataHelper
 			throw new FailedToGetFiniteAutomataFromStringListException(
 					"Failed to get finite automata from list.", exception);
 		}
-
+		
 		return finiteAutomata;
 	}
 }
--- src/finite_automata/Main.java
+++ src/finite_automata/Main.java
@@ -4,84 +4,41 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import finite_automata.Helpers.ExceptionHelper;
 import finite_automata.Helpers.FiniteAutomataHelper;
 
 public class Main
 {
-	private static IFiniteAutomata finiteAutomata;
-
-	private static Set<Transition> visited = new HashSet<Transition>();
-
-	private static void dfs(Transition transition, String word)
-	{
-		if (!Main.visited.contains(transition))
-		{
-			Main.visited.add(transition);
-		}
-
-		word += transition.getCharacter();
-
-		if (Main.finiteAutomata.getFiniteStates().contains(
-				transition.getState()))
-		{
-			System.out.println(word);
-		}
-
-		List<Transition> children = new ArrayList<Transition>();
-
-		Map<Integer, List<Transition>> transitionsMap = Main.finiteAutomata
-				.getTransitionsMap();
-
-		List<Transition> stateTransitions = transitionsMap.get(transition
-				.getState());
-
-		for (Transition childTransition : stateTransitions)
-		{
-			if (!Main.visited.contains(childTransition))
-			{
-				children.add(childTransition);
-			}
-		}
-
-		for (Transition childTransition : children)
-		{
-			Main.dfs(childTransition, word);
-		}
-
-		Main.visited.clear();
-	}
-
 	public static void main(String[] args)
 	{
 		try
 		{
 			Path path = Paths.get(args[0]);
-
+			
 			List<String> lines = Files.readAllLines(path,
 					StandardCharsets.UTF_8);
-
-			Main.finiteAutomata = FiniteAutomataHelper
+			
+			IFiniteAutomata finiteAutomata = FiniteAutomataHelper
 					.getFiniteAutomataFromStringList(lines);
-
+			
 			for (String string : FiniteAutomataHelper
-					.convertFiniteAutomataToList(Main.finiteAutomata))
+					.convertFiniteAutomataToList(finiteAutomata))
 			{
 				System.out.println(string);
 			}
+			
+			List<String> words = FiniteAutomataHelper
+			.getAllAcceptedWords(finiteAutomata);
+			
+			System.out.println();
+			System.out.println(String.format("List of the words, accepted by the automata (%1$d word(s) found):", words.size()));
 
-			System.out.println("===");
-
-			Transition initialTransition = new Transition(
-					Main.finiteAutomata.getInitialState(), '*');
-
-			Main.dfs(initialTransition, "");
+			for (String word : words)
+			{
+				System.out.println(word);
+			}
 		}
 		catch (Exception exception)
 		{
